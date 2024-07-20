41de0c8c7871e7b65ccc68c2f69a1b71da600370
Hitesh-ed <hiteshkv1503@gmail.com>
2024-07-20 21:54:50 +0530

    Update AHB_master.v

--- AHB_master.v
+++ AHB_master.v
@@ -25,59 +25,6 @@ begin   #21;
 end
 endtask
 
-task b_write();
-begin   #21;
-	Hresetn = 1;
-	Htrans = 2'b10;
-	Hreadyin = 1;
-	Haddr = 32'h80000001;
-	Hwrite = 1;
-	
-
-	#20;
-	Htrans = 2'b11;
-	Hreadyin = 1;
-	Haddr = 32'h80000002;
-	Hwdata = 32'h1234;
-	Hwrite = 1;
-	
-	#20;
-	Htrans = 2'b11;
-	Hreadyin = 1;
-	Haddr = 32'h80000003;
-	Hwdata = 32'h1235;
-	Hwrite = 1;
-
-	#40;
-	Htrans = 2'b11;
-	Hreadyin = 1;
-	Haddr = 32'h80000004;
-	Hwdata = 32'h1236;
-	Hwrite = 1;
-	
-	#40;
-	Htrans = 2'b00;
-	Hreadyin = 0;
-	Hwdata = 32'h1237;
-	Hwrite = 0;
-	
-end
-endtask
-
-task s_read();
-begin   #21;
-	Hresetn = 1;
-	Htrans = 2'b11;
-	Hreadyin = 1;
-	Haddr = 32'h80004001;
-	Hwrite = 0;
-	
-	#40;
-	Htrans = 2'b00;
-	Hreadyin = 0;
-
-	#20;
-	Hwrite = 1;
 	
 end
 endtask
